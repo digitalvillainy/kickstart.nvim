aa4d87869699866631da9f261d937d1890523e5c
rrivera <rrivera@redbannermedia.com>
2025-02-22 13:20:00 -0500

    removed sa option

--- init.lua
+++ init.lua
@@ -326,13 +326,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sa', function()
-        require('telescope.builtin').live_grep {
-          additional_args = function()
-            return { '--hidden', '--no-ignore' }
-          end,
-        }
-      end, { desc = '[S]earch by [G]rep [G]lobal (Ignores .gitignore)' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
