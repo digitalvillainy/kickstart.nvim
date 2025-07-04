0bed671203489159bdf289af0c2951dfab8fb7b4
brivera <rrivera@redbannermedia.com>
2025-07-04 00:42:23 -0400

    Fixed issues with error message. needed setup not .toggle_quick_menu

--- lua/custom/plugins/buffer_manager.lua
+++ lua/custom/plugins/buffer_manager.lua
@@ -2,7 +2,12 @@ return {
   "j-morano/buffer_manager.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("buffer_manager").toggle_quick_menu()
+    require("buffer_manager").setup {
+      -- auto_close = true,
+      -- auto_close_all = true,
+      -- auto_close_all_ignore_pattern = ".*",
+      -- auto_close_all_ignore_filetype = ".*",
+    }
   end,
 
   vim.keymap.set("n", "<leader>bm", function()
